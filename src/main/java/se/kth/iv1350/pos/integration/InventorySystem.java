d1d5beeee11ab9280617fa1248f38004cbab3e9d
GUSSCH <steamjg04@gmail.com>
2024-04-25 12:37:45 +0200

    .

--- src/main/java/se/kth/iv1350/pos/integration/InventorySystem.java
+++ src/main/java/se/kth/iv1350/pos/integration/InventorySystem.java
@@ -13,9 +13,18 @@ import se.kth.iv1350.pos.model.Item;
 *
  */
 public class InventorySystem {
-    
+    private Item[] storeItems = new Item[10];
+    private ItemDTO[] itemDTOs = new ItemDTO[10];
+
     public InventorySystem(){
         
+    }
+
+    public void addItem() {
+        this.storeItems[0] = new Item(1, new ItemDTO("Milk", 10, 2), 10);
+        this.storeItems[1] = new Item(2, new ItemDTO("Bread", 20, 3), 10);
+        this.itemDTOs[0] = new ItemDTO("Milk", 10, 2);
+        this.itemDTOs[1] = new ItemDTO("Bread", 20, 3);
     }
     
     public Item fetchItem(int itemID){
