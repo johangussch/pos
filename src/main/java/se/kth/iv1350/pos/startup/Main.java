ef4e15d38e2a174ff7a784c11cd8d504f7d24a1f
Ante <andreas.fernandez@hotmail.com>
2024-04-26 16:58:24 +0200

    tests

--- src/main/java/se/kth/iv1350/pos/startup/Main.java
+++ src/main/java/se/kth/iv1350/pos/startup/Main.java
@@ -16,8 +16,6 @@ public class Main {
   
     public static void main(String[] args){
         Controller contr = new Controller();
-        View view = new View(contr);
-        
-        view.runFakeExecution();
+        View view = new View(contr);   
     }
 }
---
+++ src/test/java/se/kth/iv1350/pos/controller/ControllerTest.java
@@ -0,0 +1,14 @@
+package se.kth.iv1350.pos.controller;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+import java.util.List;
+import se.kth.iv1350.pos.integration.*;
+import se.kth.iv1350.pos.model.*;
+
+public class ControllerTest {
+    
+}
